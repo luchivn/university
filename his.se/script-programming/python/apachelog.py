92fdf3dd7306676793ca40740546de1438f0a159
Alexei <alexeiluchian9@gmail.com>
2025-05-01 20:02:25 +0200

    Update apachelog.py

--- his.se/script-programming/python/apachelog.py
+++ his.se/script-programming/python/apachelog.py
@@ -3,7 +3,7 @@
 # USERNAME: f24alelu
 # COURSE: Script Programming IT384G - Spring 2025
 # ASSIGNMENT: Assignment 1 - Python - Task 4
-# DATE OF LAST CHANGE: 2025-04-29
+# DATE OF LAST CHANGE: 2025-05-01
 #-----------------------------------------------
 
 import re
@@ -19,8 +19,6 @@ def aggregatelog(filename):
     # in the Apache log file.
     ip_pattern = r"\d{1,3}(\.\d{1,3}){3}"
     method_pattern = r'"(\w+)'
-    unique_ips = 0
-    most_popular_apps = 0
     with open(filename) as file:
         for line in file:
             #getting ips data
@@ -28,12 +26,8 @@ def aggregatelog(filename):
             ip = ip.group()
             if ip in keyvaluepairscounter["IPaddress"]:
                 keyvaluepairscounter["IPaddress"][ip] += 1
-                if int(keyvaluepairscounter["IPaddress"][ip]) > most_popular_apps:
-                    most_popular = ip
-                    most_popular_apps = keyvaluepairscounter["IPaddress"][most_popular]
             else:
                 keyvaluepairscounter["IPaddress"][ip] = 1
-                unique_ips += 1
             #getting https methods data
             method = re.search(method_pattern,line)
             if method:
@@ -42,14 +36,5 @@ def aggregatelog(filename):
                     keyvaluepairscounter["Method"][method] += 1
                 else:
                     keyvaluepairscounter["Method"][method] = 1
-    #sort the methods in descendant order of occurences
-    keyvaluepairscounter["Method"] = dict(sorted(keyvaluepairscounter["Method"].items(),key=lambda item: item[1],reverse=True))
-    #print the results
-    for i in keyvaluepairscounter["Method"]:
-        print(f"{i}:",keyvaluepairscounter["Method"][i])
-    print(f"Most popular IP address id {most_popular} with {most_popular_apps} occurrences")
-    print("Number of unique IP addresses:", unique_ips)
 
     return keyvaluepairscounter
-
-dictio = aggregatelog('access-small.log')
